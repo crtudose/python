6f97aec77b1e28c84dd558794bf78ce37ae96e3a
crtudose <127765940+crtudose@users.noreply.github.com>
2023-04-02 14:46:09 +0300

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -3,3 +3,4 @@ lowtaxland_rate = 0.05
 ripoffland_rate = 0.43
  
 print('Your income is', income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You would save', income * ripoffland_rate - income * lowtaxland_rate, 'by paying taxes in Lowtaxland!')
+# Your income is 250000 and you would pay 12500.0 income tax in Lowtaxland or 107500.0 income tax in Ripoffland. You would save 95000.0 by paying taxes in Lowtaxland!
